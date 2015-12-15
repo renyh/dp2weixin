1359ef18ad6f8be73354766f51bd594d8f70b76a
renyh1013 <renyh1013@163.com>
2015-12-15 23:43:18 +0800

    将dp2CirculationWeb加到解决方案，因为他们共用底层类库

---
+++ dp2CirculationWeb/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using dp2CirculationWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dp2CirculationWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            bool bRet = true;
+            // 进行登录校验
+
+            //进行登录
+
+            // 存在Session中
+            Session["AcountName"] = model.UserName;
+
+            if (bRet == true)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            // 如果我们进行到这一步时某个地方出错，则重新显示表单
+            ModelState.AddModelError("", "提供的用户名或密码不正确。");
+            return View(model);
+        }
+
+        //
+        // POST: /Account/LogOff
+        [HttpPost]
+        public ActionResult LogOff()
+        {
+            //WebSecurity.Logout();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ dp2CirculationWeb/Controllers/ChargingController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dp2CirculationWeb.Controllers
+{
+    public class ChargingController : Controller
+    {
+        //
+        // GET: /Charging/
+
+        public ActionResult Main()
+        {
+            /*
+            string accountName = "";
+            if (Session["AcountName"] != null)
+                accountName = (string)Session["AcountName"];
+
+            if (accountName == "")
+            {
+                return this.RedirectToAction("Login", "Account");
+            }
+            */
+            return View();
+        }
+
+    }
+}
